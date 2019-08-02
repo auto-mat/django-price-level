385b675633b45b1481a924c3996cd9f05018ba3e
Timothy Hobbs <timothy@hobbs.cz>
2019-08-02 19:41:34 +0200

    Add award level model to admin

--- price_level/admin.py
+++ price_level/admin.py
@@ -7,5 +7,22 @@ from . import models
 class PriceLevelAdmin(admin.ModelAdmin):
     """Admin model for PriceLevels."""
 
-    list_display = ('name', 'pricable', 'price', 'category', 'takes_effect_on')
+    list_display = ('name', 'award_level', 'pricable', 'price', 'category', 'takes_effect_on')
     list_filter = ('pricable', 'category')
+    readonly_fields = (
+        'author',
+        'updated_by',
+    )
+
+
+@admin.register(models.AwardLevel)
+class AwardLevelAdmin(admin.ModelAdmin):
+    """Admin model for AwardLevels."""
+
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+    readonly_fields = (
+        'author',
+        'updated_by',
+    )
+
